6427e46fe564d2b83321d0f292dbf972886a14d4
ayush458pandey <ayush458pandey@gmail.com>
2026-01-04 19:46:09 +0530

    Disable strict auth to fix Guest Cart 403 error

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -147,10 +147,9 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
-        # 'rest_framework_simplejwt.authentication.JWTAuthentication', <--- REMOVED TO FIX CRASH
-    ],
+    # 🚨 CHANGE: Empty list allows Guest Cart to work without 403 CSRF errors
+    'DEFAULT_AUTHENTICATION_CLASSES': [], 
+    
     'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.AllowAny'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
