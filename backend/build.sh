472d1ee522ca758414ae334741557a69692f6990
ayush458pandey <ayush458pandey@gmail.com>
2026-01-26 15:32:47 +0530

    Fake migration 0011 to fix deploy

--- backend/build.sh
+++ backend/build.sh
@@ -9,6 +9,8 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations 
+# First fake migration 0011 since has_variants column already exists in production
+python manage.py migrate products 0011 --fake || true
 python manage.py migrate
 
 # Add this line to create the superuser automatically
