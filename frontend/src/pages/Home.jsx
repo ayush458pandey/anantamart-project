bde98ef3d2c0f616cb07175b07ea826bcd3eb2f3
Ayush Pandey <ayush458pandey@gmail.com>
2026-09-14 22:49:04 +0530

    fix: add missing closing div in Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -943,6 +943,7 @@ export default function Home() {
                                 </div>
                             </div>
                             </div>
+                            </div>
                         </div>
                     )}
                 </>
